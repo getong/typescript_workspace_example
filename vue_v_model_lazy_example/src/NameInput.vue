7723e589879e18d50e87ab77d6ce308237430929
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-06-04 03:34:53 +0800

    add vue v_model lazy example

---
+++ vue_v_model_lazy_example/src/NameInput.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <label for="name">
+      Write your name:
+      <input
+        v-model.lazy="name"
+        placeholder="Enter your name"
+        id="name"
+      />
+    </label>
+    <p>Your name is: {{ name }}</p>
+  </div>
+</template>
+
+<script>
+ export default {
+   data() {
+     return {
+       name: '',
+     };
+   },
+ };
+</script>
+
+<style scoped>
+ /* Add any component-specific styles here */
+ label {
+   display: flex;
+   flex-direction: column;
+   margin-bottom: 10px;
+ }
+
+ input {
+   margin-top: 5px;
+   padding: 5px;
+ }
+</style>
---
+++ vue_v_model_lazy_example/src/main.ts
@@ -0,0 +1,7 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+// import App from './App.vue'
+import NameInput from './NameInput.vue';
+
+createApp(NameInput).mount('#app')
