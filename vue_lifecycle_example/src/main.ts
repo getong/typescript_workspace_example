38f5852bdf443749ad7c2a7917c014b881a818d0
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-06-04 05:33:43 +0800

    add vue lifecycly example

---
+++ vue_lifecycle_example/src/main.ts
@@ -0,0 +1,7 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+// import App from './App.vue'
+import LifeCycle from './components/LifeCycle.vue';
+
+createApp(LifeCycle).mount('#app')
