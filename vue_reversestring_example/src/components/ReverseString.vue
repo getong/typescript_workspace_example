46db045245b9b3368679e3f9d16d4c2edfe7fada
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-06-04 14:53:46 +0800

    add vue reverse string example

---
+++ vue_reversestring_example/src/components/ReverseString.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'ReversedMessage',
+  data() {
+    return {
+      message: '',
+    };
+  },
+  methods: {
+    reverseMessage(): string {
+      return this.message.split('').reverse().join('')
+    }
+  },
+});
+</script>
+
+<template>
+  <h2 class="heading">I am {{ reverseMessage() }}</h2>
+  <input v-model="message" type="text" placeholder="Enter your message" />
+</template>
