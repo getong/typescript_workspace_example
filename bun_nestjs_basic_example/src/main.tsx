738f9529db54abf8f41ebd80476cd36f1d84e5c7
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-04-25 01:25:31 +0800

    add nestjs library

--- bun_nestjs_basic_example/src/main.tsx
+++ bun_nestjs_basic_example/src/main.tsx
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import { NestFactory } from "@nestjs/core";
+import { UsersModule } from "./users/users.module";
+
+async function bootstrap() {
+  const users_modules = await NestFactory.create(UsersModule);
+  await users_modules.listen(3000);
+}
+
+bootstrap();
